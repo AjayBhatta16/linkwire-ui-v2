7e7e450731eb5d1b479828964740443e3117a36a
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2026-01-11 00:12:16 -0500

    landing page working

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,7 +1,12 @@
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
     selector: 'linkwire-landing-page',
-    template: '<p>Landing page</p>',
+    templateUrl: './landing-page.component.html',
+    styleUrls: ['./landing-page.component.css'],
+    imports: [
+        RouterModule,
+    ],
 })
 export class LandingPageComponent {}
