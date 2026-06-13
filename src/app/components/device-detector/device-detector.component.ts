8f2bfbffa2f7038e1fd7cbfb1e6c98419f05f4af
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-06-12 22:46:43 -0400

    2. Added device detector form layout

--- src/app/components/device-detector/device-detector.component.ts
+++ src/app/components/device-detector/device-detector.component.ts
@@ -1,7 +1,34 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
     selector: 'linkwire-device-detector',
-    template: '<h3>Device Detector</h3>'
+    templateUrl: './device-detector.component.html',
+    styleUrls: ['./device-detector.component.css'],
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        ReactiveFormsModule,
+    ],
 })
-export class DeviceDetectorComponent {}
+export class DeviceDetectorComponent {
+    form: FormGroup = new FormGroup({
+        userAgent: new FormControl('', { validators: [Validators.required] }),
+    });
+
+    onSubmit() {
+        const { userAgent } = this.form.value;
+
+        this.detectDevice(userAgent);
+    }
+
+    detectDevice(userAgent: string) {
+
+    }
+}
