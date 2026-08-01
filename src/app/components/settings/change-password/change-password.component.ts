112a7aa4f9ffe06f45794da4a38424475fd14ece
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-07-31 21:18:06 -0400

    2. Added blank change-password page

---
+++ src/app/components/settings/change-password/change-password.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'linkwire-change-password',
+    template: '<p>SubRouting Works!</p>',
+})
+export class ChangePasswordComponent {}
