21f30c91a58bfadb344e8f641cfb1452bed6a790
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-09-20 02:54:06 -0400

    7. Created header component

---
+++ src/app/components/app-footer/app-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+@Component({
+    selector: 'linkwire-app-footer',
+    templateUrl: './app-footer.component.html',
+    imports: [
+        RouterModule,
+    ],
+})
+export class AppFooterComponent {}
